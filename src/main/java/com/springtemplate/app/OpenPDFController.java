fafaa9f38c7b0adeb62c950cd173090583af1077
June Dela Cruz <junedelacruz@outlook.com>
2016-08-28 07:18:59 +0000

    OpenPDFController.java edited online with Bitbucket

--- src/main/java/com/springtemplate/app/OpenPDFController.java
+++ src/main/java/com/springtemplate/app/OpenPDFController.java
@@ -1,6 +1,10 @@
 package com.springtemplate.app;
 
+import java.io.ByteArrayInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -11,6 +15,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.apache.http.HttpResponse;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
@@ -121,5 +133,72 @@ public class OpenPDFController {
 			e.printStackTrace();
 		}
 	}
+	
+	@RequestMapping("/openapdfinurl")
+	public  void generatePdf( HttpServletRequest request, HttpServletResponse response) {
+		String url = "https://www.immigration.govt.nz/documents/forms-and-guides/acceptableformslist.pdf";
+		
+
+	
+		try {
+
+			HttpUriRequest urirequest;
+			urirequest = new HttpGet(url.trim());
+			HttpClient client = new DefaultHttpClient();
+
+			urirequest.addHeader("content-type", "application/pdf");			
+			HttpResponse httpresponse =	client.execute(urirequest);	
+			byte[] data = EntityUtils.toByteArray(httpresponse.getEntity());
+
+
+			if (data != null){
+				sendBytes(response, data, "application/pdf", data.length, "Invoice_111.pdf");
+			}
+
+		} catch (Exception e) {		
+			logger.error ("generatePDF " + e.getMessage());		
+		}
+
+		
+
+	}
+
+	public static void sendBytes(HttpServletResponse resp, byte[] in, String mimeType, int length, String fileName) throws FileNotFoundException, IOException {
+		ByteArrayInputStream inputStream = new ByteArrayInputStream(in); 
+		sendStream(resp, inputStream, mimeType, length, fileName);
+	}
+
+	public static void sendStream(HttpServletResponse resp, InputStream in, String mimeType, int length, String fileName) throws FileNotFoundException, IOException {
+		OutputStream out = null;
+		try {
+			if (in != null) {
+				
+		        // Set content type
+		        resp.setContentType(mimeType);
+		        // Set content size
+		        resp.setContentLength(length);
+		        // Set the file name
+		        if(fileName!=null){
+		        	resp.setHeader("Content-Disposition", "attachment; filename=\"" + fileName + "\"");
+		        }
+		        // Open the file and output streams
+		        out = resp.getOutputStream();
+		    
+		        // Copy the contents of the file to the output stream
+		        byte[] buf = new byte[1024];
+		        int count = 0;
+		        while ((count = in.read(buf)) >= 0) {
+		            out.write(buf, 0, count);
+		        }
+		        out.flush();
+		        out.close();
+			}
+		}
+		finally {
+			try {in.close();}catch (Exception ex) { }
+			try {out.close();}catch (Exception ex) { }
+		}	
+	}
+
 
 }
